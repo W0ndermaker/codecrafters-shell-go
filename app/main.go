5ab3cc7fb100645169ef0af9579ad72f10807507
w0ndermaker <ravshanyusupov02@mail.ru>
2026-05-19 14:50:44 +0500

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -9,11 +9,14 @@ import (
 
 // bufio.Reader has peek() method that
 func main() {
-	fmt.Print("$ ")
+	// REPL
+	for {
+		fmt.Print("$ ")
 
-	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	if err != nil {
-		log.Fatal("input error:", err)
+		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			log.Fatal("input error:", err)
+		}
+		fmt.Println(command[:len(command)-1] + ": command not found")
 	}
-	fmt.Println(command[:len(command)-1] + ": command not found")
 }
