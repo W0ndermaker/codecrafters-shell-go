97899850687a0011b703354cd3c7626ecb8eba71
w0ndermaker <ravshanyusupov02@mail.ru>
2026-05-19 17:33:06 +0500

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -17,19 +17,36 @@ func main() {
 			log.Fatal("input error:", err)
 		}
 
-		args := strings.Split(strings.TrimSpace(input), " ")
+		args := strings.Fields(input)
 		command := args[0]
 		args = args[1:]
 
 		switch command {
-
 		case "echo":
 			fmt.Println(strings.Join(args, " "))
+		case "type":
+			typeCommand(args)
 		case "exit":
 			os.Exit(0)
 		default:
 			fmt.Println(command + ": command not found")
 		}
 
+	}
+}
+
+func typeCommand(args []string) {
+	builtinCommands := map[string]interface{}{
+		"echo": nil,
+		"exit": nil,
+		"type": nil,
+	}
+	for _, arg := range args {
+		trimmed := strings.TrimSpace(arg)
+		if _, ok := builtinCommands[trimmed]; !ok {
+			fmt.Printf("%v: not found\n", arg)
+			continue
+		}
+		fmt.Printf("%v is a shell builtin\n", arg)
 	}
 }
